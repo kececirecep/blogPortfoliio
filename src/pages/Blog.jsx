a557769cd4a043cb9f6255ff5858ce52085d207c
kececirecep <kececirecep@outlook.com>
2023-11-28 02:51:36 +0300

    responsive

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -8,10 +8,10 @@ const Blog = () => {
   return (
     <div className=''>
       <div className='flex flex-col justify-center items-center py-8'>
-        <h2 className='font-bold text-4xl'><span className='specialColor'>Posts</span> Examples</h2>
-        <p className='py-6 text-[#CBD5E1] text-[20px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum.</p>
+        <h2 className='font-bold lg:text-4xl text-[20px]'><span className='specialColor'>Posts</span> Examples</h2>
+        <p className='py-6 text-[#CBD5E1] lg:text-[20px] text-[10px] text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, adipisci!</p>
       </div>
-      <div className='flex flex-wrap'>
+      <div className='flex lg:flex-row flex-col flex-wrap'>
         {Data.map((blog) => (
           <div key={blog.id} className='w-1/4 py-4'>
             <Link to={`/blog/${blog.id}`}><BlogCard image={blog.image} thumbnail={blog.thumbnail} title={blog.title} date={blog.date} desc={blog.shortDescription} dangerouslySetInnerHTML={{ __html: blog.content }} /></Link>
--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -12,13 +12,13 @@ const BlogDetails = () => {
     }
 
     return (
-        <div className='mt-20'>
+        <div className='mt-0 lg:mt-20'>
             <div className='py-4'>
-                <h2 className='font-medium text-5xl text-center'>{blog.title}</h2>
+                <h2 className='font-medium text-[20px] lg:text-5xl text-center'>{blog.title}</h2>
             </div>
-            <p className='text-[#CBD5E1] text-[20px] py-8'>{blog.date}</p>
+            <p className='text-[#CBD5E1] text-[10px] lg:text-[20px] lg:py-8 pb-4 text-center lg:text-start'>{blog.date}</p>
             <div className='flex justify-start'>
-                <img src={blog.image} width="50%" className='rounded-xl' alt="" />
+                <img src={blog.image} className='rounded-xl w-[100%] lg:w-[50%]' alt="" />
             </div>
             <div dangerouslySetInnerHTML={{ __html: blog.content }} />
         </div>
