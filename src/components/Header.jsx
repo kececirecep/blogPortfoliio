02dea16233163a4955caa008fa29b8f59412806b
kececirecep <kececirecep@outlook.com>
2023-11-28 02:55:19 +0300

    header-change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,7 @@ const Header = () => {
         <nav className='flex items-center justify-between'>
             <Link to="/"><img src={Logo} alt="" className='w-[120px] lg:w-[100%]' /></Link>
             <ul className='flex lg:gap-12 lg:text-[24px] font-bold gap-2 text-[12px]'> 
-              <li><NavLink to="/blog">Blog</NavLink></li>
-              <li><NavLink to="/newsletter">Newsletter</NavLink></li>
+              <li><NavLink to="/blog">Blog</NavLink></li> 
               <li><NavLink to="https://twitter.com/yuzuksuzsauron" target='_blank'>Twitter</NavLink></li>
             </ul>
         </nav>        
